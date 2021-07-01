5c781427807a7faeaa555c6a556b2ac91500df54
dek <dakoner@gmail.com>
2021-07-01 13:44:09 -0700

    Test improvements.?

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-docker build --tag flask flask && docker rm -f flask; docker run -tid -p 80:8080 -v $(pwd)/flask/app:/app --name flask flask 
+pushd flask  && PYTHONPATH=app pytest -s && popd && docker build --tag flask flask && docker rm -f flask && docker run -tid -p 80:8080 -v $(pwd)/flask/app:/app --name flask flask && docker logs -f flask
--- flask/app/test_app.py
+++ flask/app/test_app.py
@@ -1,15 +1,30 @@
+from bs4 import BeautifulSoup
 import os
 
 import pytest
 
-from AuthApp.main import create_app
+from main import create_app
 
 @pytest.fixture
 def client():
     app = create_app() #{'TESTING': True, 'DATABASE': db_path})
 
     with app.test_client() as client:
-        # with app.app_context():
-        #     init_db()
         yield client
 
+def test_root(client):
+    rv = client.get('/')
+    assert rv.status_code == 302
+    assert rv.headers['Location'].endswith("/static/index.html")
+
+def test_missing(client):
+    rv = client.get('/thispagedoesnotexist')
+    assert rv.status_code == 404
+
+def test_index(client):
+    rv = client.get('/static/index.html')
+    assert rv.status_code == 200
+    bs = BeautifulSoup(rv.data, "html.parser")
+    assert bs.head is not None
+    assert bs.body is not None
+    
