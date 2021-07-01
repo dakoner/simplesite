973e39805cf027f2e9b483ae5d2560025f3cafca
dek <dakoner@gmail.com>
2021-07-01 09:48:06 -0700

    Clean up app, removing nginx reverse proxy and lots of extra code.

--- flask/app/main.py
+++ flask/app/main.py
@@ -1,24 +1,39 @@
-#
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import logging
 import os
 import flask_cors
-from backend.polymer_backend_app import PolymerBackendApp
+import flask
+from oauth2client import client, crypt
 
+CLIENT_ID='243774406021-jeqdr97jl8kl4dvhhpqtqrbt9kjfqqs9.apps.googleusercontent.com'
 logging.basicConfig(level=logging.DEBUG)
 
-app = PolymerBackendApp()
-flask_cors.CORS(app, resources={r"/*": {"origins": "http://gork"}})
+
+class App(flask.Flask):
+    def __init__(self, **kwargs):
+        super(App, self).__init__(__name__, **kwargs)
+        self.add_url_rule("/", view_func=self.index)
+        
+    def index(self):
+        return flask.redirect("http://gork.konerding.com/static/index.html")
+
+    def auth(self):
+        token = flask.request.form['idtoken']
+
+        try:
+            idinfo = client.verify_id_token(token, CLIENT_ID)
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise crypt.AppIdentityError("Wrong issuer.")
+            
+            # If auth request is from a G Suite domain:
+            #if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+            #    raise crypt.AppIdentityError("Wrong hosted domain.")
+        except crypt.AppIdentityError:
+                # Invalid token
+            print("invalid token")
+        userid=idinfo['sub']
+        print(idinfo)
+        print("userid=%s", userid)
+        return flask.Response('OK', status=200)
+
+app = App()
+flask_cors.CORS(app, resources={r"/*": {"origins": "http://gork.konerding.com"}})
