7a9ae328331c5425508153d2b7184099c106f249
dek <dakoner@gmail.com>
2021-07-01 15:33:47 -0700

    fix lint bugs

--- app/AuthApp.py
+++ app/AuthApp.py
@@ -1,18 +1,19 @@
 import logging
-import os
 import flask
 from oauth2client import client, crypt
 
-CLIENT_ID='243774406021-jeqdr97jl8kl4dvhhpqtqrbt9kjfqqs9.apps.googleusercontent.com'
+
+CLIENT_ID = '243774406021-jeqdr97jl8kl4dvhhpqtqrbt9kjfqqs9.apps.googleusercontent.com'
 logging.basicConfig(level=logging.DEBUG)
 
 
 class AuthApp(flask.Flask):
+
     def __init__(self, **kwargs):
         super(AuthApp, self).__init__(__name__, **kwargs)
         self.add_url_rule("/", view_func=self.index)
         self.add_url_rule("/auth", view_func=self.auth, methods=['POST'])
-        
+
     def index(self):
         return flask.redirect("http://gork.konerding.com/static/index.html")
 
@@ -23,14 +24,14 @@ class AuthApp(flask.Flask):
             idinfo = client.verify_id_token(token, CLIENT_ID)
             if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
                 raise crypt.AppIdentityError("Wrong issuer.")
-            
+
             # If auth request is from a G Suite domain:
-            #if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+            # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
             #    raise crypt.AppIdentityError("Wrong hosted domain.")
         except crypt.AppIdentityError:
-                # Invalid token
+            # Invalid token
             print("invalid token")
-        userid=idinfo['sub']
+        userid = idinfo['sub']
         print(idinfo)
         print("userid=%s", userid)
         return flask.Response('OK', status=200)
--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from AuthApp import AuthApp
 import flask_cors
 
+
 def create_app():
     app = AuthApp()
     flask_cors.CORS(app, resources={r"/*": {"origins": "http://gork.konerding.com"}})
     return app
 
+
 app = create_app()
-    
