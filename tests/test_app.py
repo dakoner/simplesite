ab55e5a5fd43ff2c595f9c6238ebe473852ac3a8
dek <dakoner@gmail.com>
2021-07-06 09:25:29 -0700

    Fix test_app for API change.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,10 @@
 from bs4 import BeautifulSoup
 import pytest
-from app import create_app
+from app import app
 
 
 @pytest.fixture
 def client():
-    app = create_app()
-
     with app.test_client() as client:
         yield client
 
